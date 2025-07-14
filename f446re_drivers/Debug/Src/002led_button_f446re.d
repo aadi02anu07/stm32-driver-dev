30d03a84cfdf4b60174c269ea4d6f9acc9ea61bc
Aditya Srivastava <aadi02anu07@gmail.com>
2025-07-15 01:21:06 +0530

    SPI commit

--- f446re_drivers/Debug/Src/002led_button_f446re.d
+++ f446re_drivers/Debug/Src/002led_button_f446re.d
@@ -1,5 +1,9 @@
 Src/002led_button_f446re.o: ../Src/002led_button_f446re.c \
  E:/DRDO\ internship/Nucleo/f446re_drivers/drivers/Inc/stm32f446xx.h \
- E:/DRDO\ internship/Nucleo/f446re_drivers/drivers/Inc/stm32f446xx_gpio_driver.h
+ E:/DRDO\ internship/Nucleo/f446re_drivers/drivers/Inc/stm32f446xx_gpio_driver.h \
+ E:/DRDO\ internship/Nucleo/f446re_drivers/drivers/Inc/stm32f446xx_spi_driver.h \
+ E:/DRDO\ internship/Nucleo/f446re_drivers/drivers/Inc/stm32f446xx.h
 E:/DRDO\ internship/Nucleo/f446re_drivers/drivers/Inc/stm32f446xx.h:
 E:/DRDO\ internship/Nucleo/f446re_drivers/drivers/Inc/stm32f446xx_gpio_driver.h:
+E:/DRDO\ internship/Nucleo/f446re_drivers/drivers/Inc/stm32f446xx_spi_driver.h:
+E:/DRDO\ internship/Nucleo/f446re_drivers/drivers/Inc/stm32f446xx.h:
---
+++ f446re_drivers/Debug/drivers/Src/stm32f446xx_spi_driver.d
@@ -0,0 +1,12 @@
+drivers/Src/stm32f446xx_spi_driver.o: \
+ ../drivers/Src/stm32f446xx_spi_driver.c \
+ E:/DRDO\ internship/Nucleo/f446re_drivers/drivers/Inc/stm32f446xx_spi_driver.h \
+ E:/DRDO\ internship/Nucleo/f446re_drivers/drivers/Inc/stm32f446xx.h \
+ E:/DRDO\ internship/Nucleo/f446re_drivers/drivers/Inc/stm32f446xx_gpio_driver.h \
+ E:/DRDO\ internship/Nucleo/f446re_drivers/drivers/Inc/stm32f446xx.h \
+ E:/DRDO\ internship/Nucleo/f446re_drivers/drivers/Inc/stm32f446xx_spi_driver.h
+E:/DRDO\ internship/Nucleo/f446re_drivers/drivers/Inc/stm32f446xx_spi_driver.h:
+E:/DRDO\ internship/Nucleo/f446re_drivers/drivers/Inc/stm32f446xx.h:
+E:/DRDO\ internship/Nucleo/f446re_drivers/drivers/Inc/stm32f446xx_gpio_driver.h:
+E:/DRDO\ internship/Nucleo/f446re_drivers/drivers/Inc/stm32f446xx.h:
+E:/DRDO\ internship/Nucleo/f446re_drivers/drivers/Inc/stm32f446xx_spi_driver.h:
