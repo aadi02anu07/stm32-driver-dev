618db0112356efcc1272310587b4d68c613093d9
Aditya Srivastava <aadi02anu07@gmail.com>
2025-07-16 14:51:55 +0530

    Initial commit

--- f446re_drivers/Debug/Src/007spi_txonly_arduino.d
+++ f446re_drivers/Debug/Src/007spi_txonly_arduino.d
@@ -1,4 +1,4 @@
-Src/006spi_tx_testing.o: ../Src/006spi_tx_testing.c \
+Src/007spi_txonly_arduino.o: ../Src/007spi_txonly_arduino.c \
  E:/DRDO\ internship/Nucleo/f446re_drivers/drivers/Inc/stm32f446xx.h \
  E:/DRDO\ internship/Nucleo/f446re_drivers/drivers/Inc/stm32f446xx_gpio_driver.h \
  E:/DRDO\ internship/Nucleo/f446re_drivers/drivers/Inc/stm32f446xx_spi_driver.h \
